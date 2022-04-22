5413a3631dba940d3fe865ccd7b871c1570b31f2
Buhary Asmath <asmath.info@gmail.com>
2022-04-22 18:35:16 +0530

    React installed

--- server.js
+++ server.js
@@ -7,6 +7,6 @@ const connectDB = require('./database/db');
 
 connectDB();
 
-const port = process.env.PORT || 8000;
+const port = process.env.PORT || 3000;
 
 app.listen(port, () => console.log(` Port listening on ${port}`));
