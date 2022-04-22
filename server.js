5b2cb233bad10254584f2c7daa6e7ab02eaa7ab4
Buhary Asmath <asmath.info@gmail.com>
2022-04-22 16:18:07 +0530

    ServerListening Port assign

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`));
