47442563569ee3620c7f262105ebe35d633cb29a
Buhary Asmath <asmath.info@gmail.com>
2022-04-22 18:21:38 +0530

    DB Connected without Errors

--- database/db.js
+++ database/db.js
@@ -1,18 +1,19 @@
-const mongoose = require('mongoosse');
+const mongoose = require('mongoose');
 
 
 
 const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb+srv://user:<password>@mern-cluster.xylyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
-            UseNewUrlPaser:true,
-            UseUnifiedTopology:true
+        await mongoose.connect(
+            'mongodb+srv://user:12345@mern-cluster.xylyi.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
+            {
+                 UseNewUrlParser: true,
+                 UseUnifiedTopology: true
+            }
+        );
 
-        });
-
-        console.log('Database Connection Successfully')
-
-    }catch (err){
+        console.log('Database Connection Successfully');
+    }catch (err) {
         console.log(err);
     }
 };
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const connectDB = require('./database/db.js');
+const connectDB = require('./database/db');
 
 
 
